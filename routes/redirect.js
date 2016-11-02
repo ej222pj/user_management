92029952abff906a0510e51fe69848a02c334179
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2016-11-02 05:05:01 +0100

    Added routing for login and sessions

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
 
 router.get('/', function (req, res) {
-    if (req.session.uniqueID == 'Tendollar') {
-        res.redirect('/gym');
+
+    var findDocuments = function (db, callback) {
+        // Get the documents collection
+        var collection = db.collection('documents');
+        // Find some documents
+        collection.find({ username: req.session.uniqueID }).toArray(function (err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.log(docs)
+            callback(docs);
+
+            if (docs.length == 0) {
+                res.redirect('/');
+            }
+            else {
+                res.redirect('/gym');
+            }
+        });
     }
-    else {
-        res.redirect('/');
-    }
+
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to registration server");
+
+        findDocuments(db, function () {
+            db.close();
+        });
+    });
 });
 
 module.exports = router;
